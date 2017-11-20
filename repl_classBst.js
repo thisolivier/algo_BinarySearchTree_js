fcc62fb56dee982856190a6704eecd090574bf58
Olivier Butler <olivier.butler@gmail.com>
2017-11-20 10:24:23 +0000

    Added functions to calculate height and whether tree is balanced. Helper function expanded to generate balanced trees for testing

--- repl_classBst.js
+++ repl_classBst.js
@@ -34,15 +34,47 @@ Bst.prototype.add = (val, bst) => {
     return bst
 }
 
-let myBst = new Bst()
-
-for (let index = 0; index < 20; index += 1){
-    let newNum = Math.floor(Math.random()*300) 
-    if (Math.floor(Math.random()+0.5) == 1){
-        newNum *= -1
+Bst.prototype.tallness = (bst) => {
+    const recurse = (node) => {
+        if (node === null){ return 0 }
+        const leftDepth = node.left ? recurse(node.left) : 0
+        const rightDepth = node.right ? recurse(node.right) : 0
+        return(leftDepth > rightDepth ? leftDepth : rightDepth) + 1
     }
-    myBst.add(newNum, myBst)
+    return recurse(bst.root)
 }
+
+Bst.prototype.isBalanced = (bst) => {
+    let allGood = true
+    const recurse = (node) => {
+        if (node === null || allGood == false){ 
+            return 0 
+        }
+        const leftDepth = node.left ? recurse(node.left) : 0
+        const rightDepth = node.right ? recurse(node.right) : 0
+        if(Math.abs(leftDepth - rightDepth) > 1){
+            allGood = false
+        }
+        return(leftDepth > rightDepth ? leftDepth : rightDepth) + 1
+    }
+    recurse(myBst.root)
+    return allGood
+}
+
+let myBst
+
+do {
+    myBst = new Bst()
+    for (let index = 0; index < 10; index += 1){
+        let newNum = Math.floor(Math.random()*300) 
+        // Generate negatives too
+        if (Math.floor(Math.random()+0.5) == 1){
+            newNum *= -1
+        }
+        myBst.add(newNum, myBst)
+    }
+} while (!myBst.isBalanced(myBst))
+
 
 let context = repl.start().context
 context.Bst = Bst
